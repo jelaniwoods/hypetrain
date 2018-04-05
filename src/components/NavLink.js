bfe6f77690c7ddae5674639c614b79d10db5cdc1
Windows Letgoner <jmw686@nyu.edu>
2018-04-05 14:34:03 -0500

    move file and fix imports

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-
+import { Link } from 'react-router-dom';
 const NavLink = (props) => (
     <li style={styles.navItem}>
       <Link {...props} style={{ color: 'inherit' }}/>
     </li>
 )
-
+let styles = {};
 styles.navItem = {
     textAlign: 'center',
     flex: 1,
