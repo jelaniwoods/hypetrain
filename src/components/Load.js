4f14b82f38261b060324f647386239f96d1cffd0
Windows Letgoner <jmw686@nyu.edu>
2018-04-06 14:46:22 -0500

    add placeholder gif and style

--- src/components/Load.js
+++ src/components/Load.js
@@ -3,13 +3,20 @@ import React from 'react';
 const Load = () => {
     return (
       <div style={bg}>
-        <h1>Load</h1>
+        
       </div>
     )
   }
 
   let bg = {
-    opacity: .5
+    position: 'absolute',
+    top: '40px',
+    zIndex: -2,
+    width: '800px',
+    height: '600px',
+    transform: 'scaleX(-1)',
+    left: '30%',
+    background: "url(https://i.pinimg.com/originals/49/a8/5f/49a85ff2855bbce54d4229ff75fa14a2.gif)"
   }
 
   export default Load;
